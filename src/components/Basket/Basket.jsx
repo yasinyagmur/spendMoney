4e0ee9d68c805bd259fd6b59ee3bc6cfdf7518c9
yasin yağmur <yasin.yagmur@gmail.com>
2022-12-11 14:53:19 +0300

    finished

--- src/components/Basket/Basket.jsx
+++ src/components/Basket/Basket.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import BasketItem from "../BasketItem/BasketItem";
+import "./basket.css";
 
 const Basket = ({ basket, products, total, resetBasket }) => {
   return (
     <>
-      {basket.map((item) => (
-        <BasketItem
-          item={item}
-          product={products.find((p) => p.id === item.id)}
-        />
-      ))}
-      <div>Toplam:${total}</div>
-      <button onClick={resetBasket}>Sepeti Sıfırla</button>
+      <div className="basket-container container">
+        <h3>Alışveriş Detayları</h3>
+        <ul>
+          {basket.map((item) => (
+            <BasketItem
+              key={item.id}
+              item={item}
+              product={products.find((p) => p.id === item.id)}
+            />
+          ))}
+        </ul>
+        <div className="total">Toplam:${total}</div>
+        <button className="basket-reset-btn" onClick={resetBasket}>
+          Sepeti Sıfırla
+        </button>
+      </div>
     </>
   );
 };
--- src/components/BasketItem/BasketItem.jsx
+++ src/components/BasketItem/BasketItem.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import "./basketItem.css";
 
 const BasketItem = ({ item, product }) => {
   return (
-    <div>
-      {product.title} x {item.amount}
-    </div>
+    <>
+      <li className="basket-item">
+        {product.title} <span>x {item.amount}</span>
+      </li>
+    </>
   );
 };
 
